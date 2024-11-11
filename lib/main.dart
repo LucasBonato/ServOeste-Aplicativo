f611c1c0714b9c81a4355dc124dc4b6c248f782a
lucasatdriano <lucasatdriano@gmail.com>
2024-11-11 19:15:59 -0300

    nem commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,19 +6,18 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-	const MyApp({super.key});
+  const MyApp({super.key});
 
-	@override
-	Widget build(BuildContext context) {
-		return MaterialApp(
-			title: 'ServOeste',
-			debugShowCheckedModeBanner: false,
-			theme: ThemeData(
-				colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
-				useMaterial3: true,
-			),
-			initialRoute: "/",
-			routes: CustomRouter.getRoutes(context)
-		);
-	}
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'ServOeste',
+        debugShowCheckedModeBanner: true,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+          useMaterial3: true,
+        ),
+        initialRoute: "/",
+        routes: CustomRouter.getRoutes(context));
+  }
 }
