563cfba01ff4b0890b1a899c6eab85f96a4135c2
lucasatdriano <lucasatdriano@gmail.com>
2024-11-20 19:27:32 -0300

    [BUILD] homePage and adding Components sideBarNavigation and CardService

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -1,85 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:serv_oeste/src/screens/account.dart';
 import 'package:serv_oeste/src/screens/cliente/cliente.dart';
 import 'package:serv_oeste/src/screens/servico/servico.dart';
 import 'package:serv_oeste/src/screens/tecnico/tecnico.dart';
 import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
+import 'package:serv_oeste/src/components/sidebar_navigation.dart';
+import 'package:serv_oeste/src/components/card_service.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
-  State<Home> createState() => HomeState();
+  State<Home> createState() => _HomeState();
 }
 
-class HomeState extends State<Home> {
+class _HomeState extends State<Home> {
   late PersistentTabController _persistentController;
+
   final List<ScrollController> _scrollControllers = [
     ScrollController(),
-    ScrollController()
-  ];
-  List<Widget> _paginas() => [
-    const Servico(),
-    const ClientePage(),
-    Column(
-      mainAxisSize: MainAxisSize.max,
-      children: [
-        Expanded(
-          child: Align(
-            alignment: const AlignmentDirectional(0, 0),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(6),
-              child: Image.asset(
-                'assets/servOeste.png',
-                fit: BoxFit.cover,
-                alignment: const Alignment(0, 0),
-              ),
-            ),
-          ),
-        ),
-      ],
-    ), // Home
-    const TecnicoPage(),
-    const Account()
-  ];
-  List<PersistentBottomNavBarItem> _navBarItens() => [
-    PersistentBottomNavBarItem(
-      icon: const Icon(Icons.content_paste, size: 35),
-      title: "Serviços",
-      textStyle: const TextStyle(fontSize: 15),
-      activeColorPrimary: Colors.blue,
-      inactiveColorPrimary: Colors.black,
-      scrollController: _scrollControllers.first
-    ),
-    PersistentBottomNavBarItem(
-      icon: const Icon(Icons.people_outline, size: 35),
-      title: "Clientes",
-      textStyle: const TextStyle(fontSize: 15),
-      activeColorPrimary: Colors.blue,
-      inactiveColorPrimary: Colors.black
-    ),
-    PersistentBottomNavBarItem(
-      icon: const Icon(Icons.home_outlined, size: 40),
-      title: "Home",
-      textStyle: const TextStyle(fontSize: 15),
-      activeColorPrimary: Colors.blue,
-      inactiveColorPrimary: Colors.black
-    ),
-    PersistentBottomNavBarItem(
-      icon: const Icon(Icons.build_outlined, size: 35),
-      title: "Técnicos",
-      textStyle: const TextStyle(fontSize: 15),
-      activeColorPrimary: Colors.blue,
-      inactiveColorPrimary: Colors.black
-    ),
-    PersistentBottomNavBarItem(
-        icon: const Icon(Icons.brightness_low, size: 35),
-        title: "...",
-        textStyle: const TextStyle(fontSize: 15),
-        activeColorPrimary: Colors.blue,
-        inactiveColorPrimary: Colors.black,
-        scrollController: _scrollControllers.last
-    ),
+    ScrollController(),
   ];
 
   @override
@@ -90,67 +29,240 @@ class HomeState extends State<Home> {
 
   @override
   void dispose() {
-    for(final scrollController in _scrollControllers) {
+    for (final scrollController in _scrollControllers) {
       scrollController.dispose();
     }
     super.dispose();
   }
 
-	@override
-	Widget build(BuildContext context) {
-		return Scaffold(
-      appBar: AppBar(
-        title: ClipRRect(
-          borderRadius: BorderRadius.circular(6),
-          child: Image.asset(
-            'assets/servOeste.png',
-            fit: BoxFit.cover,
-            alignment: const Alignment(0, 0),
-          ),
+  List<Widget> _buildScreens() => [
+        const Servico(),
+        const ClientePage(),
+        _buildHomeScreen(), // Substituído para refletir o design
+        const TecnicoPage(),
+      ];
+
+  List<PersistentBottomNavBarItem> _navBarItems() => [
+        _buildNavBarItem(
+          icon: Icons.content_paste,
+          title: "Serviços",
+          scrollController: _scrollControllers.first,
         ),
-        centerTitle: true,
-        backgroundColor: const Color.fromRGBO(253, 253, 253, 100),
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-            bottomRight: Radius.circular(20),
-            bottomLeft: Radius.circular(20)
-          )
+        _buildNavBarItem(
+          icon: Icons.people,
+          title: "Clientes",
+        ),
+        _buildNavBarItem(
+          icon: Icons.home,
+          title: "Home",
+        ),
+        _buildNavBarItem(
+          icon: Icons.build,
+          title: "Técnicos",
+          scrollController: _scrollControllers.last,
+        ),
+      ];
+
+  PersistentBottomNavBarItem _buildNavBarItem({
+    required IconData icon,
+    required String title,
+    double size = 40,
+    ScrollController? scrollController,
+  }) {
+    return PersistentBottomNavBarItem(
+      icon: Icon(icon, size: size),
+      title: title,
+      textStyle: const TextStyle(fontSize: 15),
+      activeColorPrimary: Colors.blue,
+      inactiveColorPrimary: Colors.black,
+      scrollController: scrollController,
+    );
+  }
+
+  Widget _buildHomeScreen() {
+    return Scaffold(
+      backgroundColor: const Color(0xFFFBFBFB),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Header ajustado
+            Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: MediaQuery.of(context).size.width * 0.05,
+                vertical: 10,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    offset: const Offset(0, 2),
+                    blurRadius: 6,
+                  ),
+                ],
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(6),
+                    child: ConstrainedBox(
+                      constraints: const BoxConstraints(
+                        minHeight: 45,
+                        maxHeight: 50,
+                      ),
+                      child: Image.asset(
+                        'assets/servOeste.png',
+                        fit: BoxFit.cover,
+                        alignment: Alignment.center,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            // Hero com overlay e imagem com maxHeight
+            Stack(
+              children: [
+                ConstrainedBox(
+                  constraints: BoxConstraints(
+                    maxHeight: 300, // Definindo a altura máxima
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.asset(
+                      'assets/heroImage3.png',
+                      width: double.infinity,
+                      fit: BoxFit.contain,
+                      alignment: Alignment.topCenter,
+                      errorBuilder: (context, error, stackTrace) => const Icon(
+                          Icons.image_not_supported,
+                          size: 100,
+                          color: Colors.grey),
+                    ),
+                  ),
+                ),
+                Positioned.fill(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Colors.transparent,
+                          Colors.black.withOpacity(0.7),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: MediaQuery.of(context).size.width * 0.05),
+              child: Text(
+                "Agenda do Dia",
+                style: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87,
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: MediaQuery.of(context).size.width > 600
+                    ? MediaQuery.of(context).size.width * 0.02
+                    : 30,
+                vertical: 10,
+              ),
+              child: GridView.builder(
+                itemCount: 10,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: MediaQuery.of(context).size.width > 1400
+                      ? 4
+                      : MediaQuery.of(context).size.width > 1000
+                          ? 3
+                          : MediaQuery.of(context).size.width > 600
+                              ? 2
+                              : 1,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                  childAspectRatio: 2,
+                ),
+                itemBuilder: (context, index) {
+                  return _buildAgendaCard();
+                },
+              ),
+            ),
+          ],
         ),
       ),
-			body: PersistentTabView(
-        context,
-        controller: _persistentController,
-        hideNavigationBarWhenKeyboardAppears: true,
-        stateManagement: false,
-        screens: _paginas(),
-        items: _navBarItens(),
-        navBarStyle: NavBarStyle.style8,
-        navBarHeight: 65,
-        bottomScreenMargin: 65,
-        animationSettings: const NavBarAnimationSettings(
-          screenTransitionAnimation: ScreenTransitionAnimationSettings(
-            animateTabTransition: true,
-            duration: Duration(milliseconds: 200),
-            screenTransitionAnimationType: ScreenTransitionAnimationType.slide
-          )
-        ),
-        padding: const EdgeInsets.symmetric(vertical: 5),
-        decoration: const NavBarDecoration(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(20),
-            topRight: Radius.circular(20)
+    );
+  }
+
+  Widget _buildAgendaCard() {
+    return const CardServiceAgenda(
+      cliente: "Jefferson Oliveira",
+      equipamento: "Geladeira - Consul",
+      tecnico: "Técnico - Lucas Adriano",
+      local: "Osasco",
+      data: "11/11/2024 - Tarde",
+      status: "Aguardando Orçamento",
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final bool isLargeScreen = screenWidth >= 768;
+
+    if (!isLargeScreen) {
+      // Modo Mobile: Ajustando para evitar dois headers
+      return Scaffold(
+        body: PersistentTabView(
+          context,
+          controller: _persistentController,
+          screens: _buildScreens(),
+          items: _navBarItems(),
+          navBarStyle: NavBarStyle.style6,
+          navBarHeight: 70,
+          decoration: const NavBarDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
+            colorBehindNavBar: Colors.transparent,
           ),
-          colorBehindNavBar: Colors.transparent,
-          boxShadow: [
-            BoxShadow(
-              color: Color.fromRGBO(0, 0, 0, 99),
-              offset: Offset(0, -5),
-              spreadRadius: -5,
-              blurRadius: 20
-            )
-          ]
+          hideNavigationBarWhenKeyboardAppears: true,
         ),
-      )
-		);
-	}
+      );
+    } else {
+      // Modo Desktop: Sidebar com header fixo
+      return Scaffold(
+        body: Row(
+          children: [
+            const SidebarNavigation(),
+            Expanded(
+              child: IndexedStack(
+                index: _persistentController.index,
+                children: _buildScreens(),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+  }
 }
