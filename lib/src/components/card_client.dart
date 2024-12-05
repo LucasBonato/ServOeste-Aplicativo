e693e8eed99e1f07315010d68354a6a250902ceb
Lucas Bonato <lucas.perez.bonato@gmail.com>
2024-12-05 16:10:40 -0300

    [FIX] OnTap event to select a card

--- lib/src/components/card_client.dart
+++ lib/src/components/card_client.dart
@@ -7,11 +7,13 @@ class CardClient extends StatelessWidget {
   final String city;
   final String street;
   final bool isSelected;
+  final void Function()? onTap;
 
   const CardClient({
     super.key,
     required this.name,
     this.phoneNumber,
+    this.onTap,
     this.cellphone,
     required this.city,
     required this.street,
@@ -22,98 +24,101 @@ class CardClient extends StatelessWidget {
   Widget build(BuildContext context) {
     final ValueNotifier<bool> isHovered = ValueNotifier(false);
 
-    return MouseRegion(
-      cursor: SystemMouseCursors.click,
-      onEnter: (_) => isHovered.value = true,
-      onExit: (_) => isHovered.value = false,
-      child: ValueListenableBuilder<bool>(
-        valueListenable: isHovered,
-        builder: (context, hovered, child) {
-          return Container(
-            margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-            padding: const EdgeInsets.all(16),
-            decoration: BoxDecoration(
-              color: isSelected
-                  ? const Color(0xFFE9E7E7)
-                  : const Color(0xFCFDFDFF),
-              borderRadius: BorderRadius.circular(12),
-              border: Border.all(
+    return GestureDetector(
+      onTap: onTap,
+      child: MouseRegion(
+        cursor: SystemMouseCursors.click,
+        onEnter: (_) => isHovered.value = true,
+        onExit: (_) => isHovered.value = false,
+        child: ValueListenableBuilder<bool>(
+          valueListenable: isHovered,
+          builder: (context, hovered, child) {
+            return Container(
+              margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
                 color: isSelected
-                    ? Colors.black38
-                    : (hovered ? Colors.black38 : Color(0xFFEAE6E5)),
-                width: isSelected ? 1.5 : 1,
+                    ? const Color(0xFFE9E7E7)
+                    : const Color(0xFCFDFDFF),
+                borderRadius: BorderRadius.circular(12),
+                border: Border.all(
+                  color: isSelected
+                      ? Colors.black
+                      : (hovered ? Colors.black38 : Color(0xFFEAE6E5)),
+                  width: 1,
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    blurRadius: 6,
+                    offset: const Offset(0, 2),
+                  ),
+                ],
               ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.1),
-                  blurRadius: 6,
-                  offset: const Offset(0, 2),
-                ),
-              ],
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 8.0),
-                  child: Text(
-                    name,
-                    style: TextStyle(
-                      fontSize:
-                          MediaQuery.of(context).size.width.clamp(16.0, 18.0),
-                      fontWeight: FontWeight.bold,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: Text(
+                      name,
+                      style: TextStyle(
+                        fontSize:
+                            MediaQuery.of(context).size.width.clamp(16.0, 18.0),
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                ),
-                const SizedBox(height: 4),
-                if (phoneNumber != null && phoneNumber!.isNotEmpty)
+                  const SizedBox(height: 4),
+                  if (phoneNumber != null && phoneNumber!.isNotEmpty)
+                    Padding(
+                      padding: const EdgeInsets.only(left: 12.0),
+                      child: Text(
+                        'Telefone Fixo: $phoneNumber',
+                        style: TextStyle(
+                          fontSize:
+                              MediaQuery.of(context).size.width.clamp(13.0, 15.0),
+                        ),
+                      ),
+                    ),
+                  if (cellphone != null && cellphone!.isNotEmpty)
+                    Padding(
+                      padding: const EdgeInsets.only(left: 12.0),
+                      child: Text(
+                        'Celular: $cellphone',
+                        style: TextStyle(
+                          fontSize:
+                              MediaQuery.of(context).size.width.clamp(13.0, 15.0),
+                        ),
+                      ),
+                    ),
+                  const SizedBox(height: 12),
                   Padding(
                     padding: const EdgeInsets.only(left: 12.0),
                     child: Text(
-                      'Telefone Fixo: $phoneNumber',
+                      '$city - SP',
                       style: TextStyle(
                         fontSize:
                             MediaQuery.of(context).size.width.clamp(13.0, 15.0),
                       ),
                     ),
                   ),
-                if (cellphone != null && cellphone!.isNotEmpty)
                   Padding(
                     padding: const EdgeInsets.only(left: 12.0),
                     child: Text(
-                      'Celular: $cellphone',
+                      street,
                       style: TextStyle(
                         fontSize:
                             MediaQuery.of(context).size.width.clamp(13.0, 15.0),
                       ),
                     ),
                   ),
-                const SizedBox(height: 12),
-                Padding(
-                  padding: const EdgeInsets.only(left: 12.0),
-                  child: Text(
-                    '$city - SP',
-                    style: TextStyle(
-                      fontSize:
-                          MediaQuery.of(context).size.width.clamp(13.0, 15.0),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 12.0),
-                  child: Text(
-                    street,
-                    style: TextStyle(
-                      fontSize:
-                          MediaQuery.of(context).size.width.clamp(13.0, 15.0),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          );
-        },
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
