b563cd99356fe7aea03894ab3509c011c50e6a3f
Lucas Bonato <lucas.perez.bonato@gmail.com>
2025-01-15 23:06:35 -0300

    [STYLE] Identation

--- lib/src/util/build_widgets.dart
+++ lib/src/util/build_widgets.dart
@@ -2,25 +2,19 @@ import 'package:flutter/material.dart';
 import 'dialog_box.dart';
 
 class BuildWidgets {
-  static FloatingActionButton buildFabAdd(
-      BuildContext context, String route, dynamic event,
-      {String tooltip = ''}) {
+  static FloatingActionButton buildFabAdd(BuildContext context, String route, dynamic event, {String tooltip = ''}) {
     return FloatingActionButton(
       heroTag: "Add_$route",
       backgroundColor: Colors.blue,
       shape: const CircleBorder(eccentricity: 0),
       elevation: 8,
       tooltip: tooltip,
-      onPressed: () => Navigator.of(context, rootNavigator: true)
-          .pushNamed(route)
-          .then((value) => value ?? event()),
+      onPressed: () => Navigator.of(context, rootNavigator: true).pushNamed(route).then((value) => value ?? event()),
       child: const Icon(Icons.add, color: Colors.white, size: 36),
     );
   }
 
-  static FloatingActionButton buildFabRemove(
-      BuildContext context, dynamic removeMethod,
-      {String tooltip = ''}) {
+  static FloatingActionButton buildFabRemove(BuildContext context, dynamic removeMethod, {String tooltip = ''}) {
     return FloatingActionButton(
       heroTag: "Remove_$tooltip",
       backgroundColor: Colors.red,
